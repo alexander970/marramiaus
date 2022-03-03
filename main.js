6bbbc26a474cf93e75e9c29a3a2b258ee5b53651
Alexander <50184502+alexander970@users.noreply.github.com>
2022-03-03 17:20:16 +0100

    Clear button functionality

--- main.js
+++ main.js
@@ -73,3 +73,21 @@ function searchMarramiaus(event) {
 }
 
 input.addEventListener("input", searchMarramiaus);
+
+
+
+const clearIcon = document.querySelector(".clear-icon");
+const searchBar = document.querySelector(".search");
+
+searchBar.addEventListener("keyup", () => {
+  if(searchBar.value && clearIcon.style.visibility != "visible"){
+    clearIcon.style.visibility = "visible";
+  } else if(!searchBar.value) {
+    clearIcon.style.visibility = "hidden";
+  }
+});
+
+clearIcon.addEventListener("click", () => {
+  searchBar.value = "";
+  clearIcon.style.visibility = "hidden";
+});
